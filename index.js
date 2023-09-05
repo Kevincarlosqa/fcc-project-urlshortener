bdee3085b6952f517ed497262a2612a94151b9d8
Kevin Quispe <kevincarlosqa@gmail.com>
2023-09-05 18:19:17 -0500

    fix & finish

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // Basic Configuration
 const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -16,13 +16,27 @@ app.get("/", function (req, res) {
 });
 
 let urlBank = [];
+
 // Your first API endpoint
 app.post("/api/shorturl", function (req, res) {
   function esURLValida(url) {
     // Expresión regular para verificar una URL válida
-    const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+    const urlRegex = /^(https?):\/\/[^\s/$.?#].[^\s]*$/;
     return urlRegex.test(url);
   }
+  // function isValidURL(inputURL) {
+  //   try {
+  //     new URL(inputURL);
+  //     return true;
+  //   } catch (error) {
+  //      res.json({ error: "invalid url" });
+  //   }
+  // }
+  // if (isValidURL(req.body.url)) {
+  //   // La URL es válida
+  //   urlBank.push(req.body.url);
+  //   res.json({ original_url: req.body.url, short_url: urlBank.length });
+  // }
   if (esURLValida(req.body.url)) {
     urlBank.push(req.body.url);
     res.json({ original_url: req.body.url, short_url: urlBank.length });
